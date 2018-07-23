f1436638d26f1187df42802adb89f7208fbae00c
Scisco <scisco7@gmail.com>
2018-07-23 14:06:18 -0400

    remove temp directory

--- node/bin/cli.js
+++ node/bin/cli.js
@@ -2,6 +2,7 @@
 
 'use strict'
 
+const fs = require('fs-extra')
 const program = require('commander')
 const pckg = require('../package.json')
 const runner = require('../index')
@@ -18,13 +19,19 @@ program
   .option('--profile <profile>', 'AWS credentials profile name')
   .option('--region <region>', 'AWS region (defaults to us-east-1)')
   .action((arn, cmd) => {
-    const event = cmd.event || {}
+    let event = {}
+    if (cmd.event) {
+      event = JSON.parse(cmd.event)
+    }
     const tempDir = runner.mkdtemp()
     const region = cmd.region || 'us-east-1'
     const profile = cmd.profile
 
     runner.configureAws(region, profile)
-    runner.invoke(event, arn, tempDir).then(console.log).catch(console.error)
+    runner.invoke(event, arn, tempDir)
+      .then(console.log)
+      .catch(console.error)
+      .then(() => fs.remove(tempDir))
   })
 
 program
