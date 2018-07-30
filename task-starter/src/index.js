91b00bcb90794eb4899158491d6e59a66d854ecd
Scisco <scisco7@gmail.com>
2018-07-30 11:56:00 -0400

    add docs

--- task-starter/src/index.js
+++ task-starter/src/index.js
@@ -2,12 +2,24 @@
 
 const AWS = require('aws-sdk')
 
+/**
+ * Error object for invalid inputs
+ *
+ * @param {String} msg - error message
+ * @returns {Error} the error object
+ */
 function newException(msg) {
   const err = new Error(msg)
   err.code = 'INVALID_INPUT'
   return err
 }
 
+/**
+ * Validate inputs to the lambda function
+ *
+ * @param {Object} event - the input to the Lambda function
+ * @returns {Error} the error object or undefined if the input is good
+ */
 function validateLambdaInput(event) {
   if (!event.arn) return newException('Lambda ARN is missing')
   if (!event.input) return newException('The input for the ecs task is not provided')
@@ -19,11 +31,16 @@ function validateLambdaInput(event) {
 
   if (!Array.isArray(event.subnets)) return newException('subnets must be an array')
   if (!Array.isArray(event.securityGroups)) return newException('securityGroups must be an array')
-  if (!Array.isArray(event.securityGroups)) return newException('securityGroups must be an array')
   if (typeof event.input !== 'object') return newException('input must be an object')
   return undefined
 }
 
+/**
+ * Gets Environment Variables of a given Lambda
+ *
+ * @param {string} arn - lambda arn address
+ * @returns {Array} an array of objects with name and value
+ */
 async function getEnvVariables(arn) {
   const lambda = new AWS.Lambda()
   const data = await lambda.getFunction({ FunctionName: arn }).promise()
@@ -40,6 +57,19 @@ async function getEnvVariables(arn) {
   return envs
 }
 
+/**
+ * Starts an Fargate ECS Task
+ *
+ * @param {Object} options - an options object
+ * @param {string} options.cluster - the ECS cluster
+ * @param {string} options.taskDefinition - the ECS taskDefinition
+ * @param {Array} options.subnets - a list of subnets that run the task on
+ * @param {Array} options.securityGroups - a list of security groups
+ * @param {string} options.arn - the lambda function arn
+ * @param {string} options.input - the input to the lambda function
+ * @param {string} options.roleArn - the IAM role to attach to the ECS task
+ * @returns {Promise<Object>} the AWS runTask ECS response
+ */
 async function startTask(options) {
   const ecs = new AWS.ECS()
   const params = {
@@ -76,6 +106,14 @@ async function startTask(options) {
   return ecs.runTask(params).promise()
 }
 
+/**
+ * Lambda Function's main handler
+ *
+ * @param {Object} event - Lambda function event object
+ * @param {Object} context - Lambda function's context object
+ * @param {Function} cb - callback function
+ * @returns {Promise} - the output of the startTask function
+ */
 function handler(event, context, cb) {
   // validate input
   const err = validateLambdaInput(event)
